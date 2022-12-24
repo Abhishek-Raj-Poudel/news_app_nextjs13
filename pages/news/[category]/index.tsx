02c2be7a7d6a3e16c546b55125aef9a768b903ed
unknown <abhishekrajpoudel@gmail.com>
2022-12-24 17:38:37 +0545

    All done and it fully working Deployment needed

--- pages/news/[category]/index.tsx
+++ pages/news/[category]/index.tsx
@@ -3,22 +3,22 @@ import React, { useEffect, useState } from "react";
 import { categories } from "../../../constants";
 import fetchNews from "../../../lib/fetchNews";
 import NewsList from "../../NewsList";
-// type Props = {
-//   params: { category: Category };
-// };
 
 function NewsCategory() {
-  const [news, setNews] = useState();
+  const [news, setNews] = useState<NewsResponse | null>(null);
   const router = useRouter();
   const asPath = router.asPath;
   const category = asPath.split("/")[2];
   //giving map error
-  // useEffect(() => {
-  //   let news = async () => await fetchNews(category);
-  //   setNews(news);
-  // });
+  useEffect(() => {
+    async function fetchData() {
+      const data: NewsResponse = await fetchNews(category);
+      setNews(data);
+    }
+    fetchData();
+  }, []);
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <h1 className="headerTitle">{category}</h1>
       <NewsList news={news ? news : null} />
     </div>
@@ -27,8 +27,8 @@ function NewsCategory() {
 
 export default NewsCategory;
 
-// export async function generateStaticParams() {
-//   return categories.map((category) => ({
-//     category: category,
-//   }));
-// }
+export async function generateStaticParams() {
+  return categories.map((category) => ({
+    category: category,
+  }));
+}
--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,40 +1,36 @@
-"use server";
-
-import Error from "next/error";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import fetchNews from "../../lib/fetchNews";
 import NewsList from "../NewsList";
 
-type Props = {
-  searchParams?: { term: string };
-};
-
-function SearchPage({ searchParams }: Props) {
-  const [news, setNews] = useState<NewsResponse | null>();
+function SearchPage() {
+  const [news, setNews] = useState<NewsResponse | null>(null);
   const router = useRouter();
   const { query } = router;
 
-  console.log(query);
+  console.log(query.term);
 
   useEffect(() => {
-    async () => {
-      let news = await fetchNews("general", "world cup", true);
-      console.log(news);
-      setNews(news);
-    };
-  });
+    async function fetchData() {
+      const data: NewsResponse = await fetchNews(
+        "general",
+        `${query.term}`,
+        true
+      );
+      setNews(data);
+    }
+    fetchData();
+  }, []);
+  // console.log(news);
 
-  console.log(news);
-
-  if ((query && Object.entries(query).length === 0) || !query) {
-    return <Error statusCode={404} />;
+  if (!news) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
       <h1 className="headerTitle">Search Result for: {query?.term}</h1>
-      <NewsList news={news ? news : null} />
+      <NewsList news={news} />
     </div>
   );
 }
